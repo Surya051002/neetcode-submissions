8cf103b578ff038cfbf70d102425fcba65d1b89c
Suryaprakash.B <100951787+Surya051002@users.noreply.github.com>
2026-09-16 07:34:57 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        res=set()
+
+        def findpre(nums,index):
+            nonlocal res
+            
+            if index==len(nums):
+                res.add(tuple(nums))
+                return
+
+            for i in range(0,len(nums)):
+                if i+1<len(nums) and nums[i]==nums[i+1]:
+                    continue
+                nums[i],nums[index]=nums[index],nums[i]
+                findpre(nums,index+1)
+                nums[i],nums[index]=nums[index],nums[i]
+        findpre(nums,0)
+        return [list(i) for i in res]
+
+            
+        
